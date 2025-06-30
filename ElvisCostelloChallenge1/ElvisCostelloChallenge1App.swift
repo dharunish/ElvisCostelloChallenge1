26ed7e857c9f980a0a5efc465203f4b8916d3468
Dharunish <dharyugi@gmail.com>
2025-06-29 20:36:19 -0400

    Initial Commit

---
+++ ElvisCostelloChallenge1/ElvisCostelloChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloChallenge1App.swift
+//  ElvisCostelloChallenge1
+//
+//  Created by Dharunish Yugeswardeenoo on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
