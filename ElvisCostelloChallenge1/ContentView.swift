f0220208db179da3b6f0d3f1198679e5369b50aa
Dharunish <dharyugi@gmail.com>
2025-06-29 20:52:06 -0400

    challenge exercise

--- ElvisCostelloChallenge1/ContentView.swift
+++ ElvisCostelloChallenge1/ContentView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var img = ""
+    @State private var message = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.purple)
+            Spacer()
+            Image(systemName: img)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.black)
+                .frame(width: 300, height: 300)
+            Text(message)
+                .font(.title)
+                .foregroundStyle(.purple)
+                .fontWeight(.bold)
+            Spacer()
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    img = "peacesign"
+                }
+                Button("Love") {
+                    message = "Love"
+                    img = "heart"
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    img = "lightbulb"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
         }
         .padding()
     }
